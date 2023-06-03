b7c22b6c3b566cf1d74036790da1dc9bcda51f41
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-06-03 21:08:21 +0200

    +task & labels works, not sure why, fix code so it makes sense tmrw

--- ToDoAssignment.js
+++ ToDoAssignment.js
@@ -42,32 +42,95 @@ newTaskButton.addEventListener("click", function () {
 });
 //NEWTASKBUTTON END
 
-//CHECKBOX STATUS AND DATE START
-document.addEventListener("DOMContentLoaded", function () {
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+// CREATE NEW TASK START
+const taskContainer = document.querySelector(".taskContainer");
+const taskForm = document.querySelector(".newTaskForm form");
 
-  const completionDates = [];
+taskForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-  checkboxes.forEach(function (checkbox, index) {
-    const completedLabel =
-      checkbox.nextElementSibling.querySelector(".completed-label");
-    const completedDate =
-      checkbox.nextElementSibling.querySelector(".completed-date");
+  const taskTitleInput = document.getElementById("enterTaskTitle");
+  const taskDescriptionInput = document.getElementById("enterTaskDescription");
 
-    checkbox.addEventListener("change", function () {
-      if (checkbox.checked) {
-        completionDates[index] = new Date().toLocaleDateString();
-        completedLabel.classList.add("visible");
-        completedDate.textContent = completionDates[index];
-        completedDate.classList.add("visible");
-      } else {
-        completionDates[index] = null;
-        completedLabel.classList.remove("visible");
-        completedDate.textContent = "";
-        completedDate.classList.remove("visible");
-      }
-    });
-  });
+  const taskTitle = taskTitleInput.value;
+  const taskDescription = taskDescriptionInput.value;
+
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task");
+
+  const statusDiv = document.createElement("div");
+  statusDiv.classList.add("status");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "task-" + (taskContainer.childElementCount + 1);
+  statusDiv.appendChild(checkbox);
+
+  const label = document.createElement("label");
+  label.htmlFor = "task-" + (taskContainer.childElementCount + 1);
+  statusDiv.appendChild(label);
+
+  const completedLabel = document.createElement("span");
+  completedLabel.classList.add("completed-label");
+  completedLabel.textContent = "Completed ";
+  label.appendChild(completedLabel);
+
+  const completedDate = document.createElement("span");
+  completedDate.classList.add("completed-date");
+  label.appendChild(completedDate);
+
+  taskElement.appendChild(statusDiv);
+
+  const taskTitleElement = document.createElement("p");
+  taskTitleElement.classList.add("taskTitle");
+  taskTitleElement.textContent = taskTitle;
+  taskElement.appendChild(taskTitleElement);
+
+  const taskDescriptionElement = document.createElement("p");
+  taskDescriptionElement.classList.add("description");
+  taskDescriptionElement.textContent = taskDescription;
+  taskElement.appendChild(taskDescriptionElement);
+
+  const createdDateElement = document.createElement("time");
+  createdDateElement.classList.add("createdDate");
+  const currentDate = new Date();
+  const formattedDate = `${currentDate.getDate()}/${
+    currentDate.getMonth() + 1
+  }-${currentDate.getFullYear()}`;
+  createdDateElement.textContent = formattedDate;
+  taskElement.appendChild(createdDateElement);
+
+  const editButton = document.createElement("button");
+  editButton.classList.add("button", "edit");
+  taskElement.appendChild(editButton);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("button", "delete", "task");
+  taskElement.appendChild(deleteButton);
+
+  taskContainer.insertBefore(taskElement, newTaskForm);
+
+  taskTitleInput.value = "";
+  taskDescriptionInput.value = "";
+  newTaskForm.style.display = "none";
 });
 
-//CHECKBOX STATUS AND DATE END
+// CHECKBOX STATUS AND DATE START
+document.addEventListener("change", function (event) {
+  if (event.target.matches('input[type="checkbox"]')) {
+    const checkbox = event.target;
+    const taskElement = checkbox.closest(".task");
+    const completedLabel = taskElement.querySelector(".completed-label");
+    const completedDate = taskElement.querySelector(".completed-date");
+
+    if (checkbox.checked) {
+      completedLabel.classList.add("visible");
+      completedDate.textContent = new Date().toLocaleDateString();
+      completedDate.classList.add("visible");
+    } else {
+      completedLabel.classList.remove("visible");
+      completedDate.textContent = "";
+      completedDate.classList.remove("visible");
+    }
+  }
+});
